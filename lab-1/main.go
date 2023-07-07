5476af2bd777234dcba4ef192c49aca9077f46ea
khangnh3 <khangnh3@hdbank.com.vn>
2023-07-07 14:29:51 +0700

    lab:crud splice student

--- lab-1/main.go
+++ lab-1/main.go
@@ -1,13 +1,27 @@
 package main
 import (
-    "fmt"
-
-    // import from package
+    _"fmt"
      "go-tutorial/model"
+     "go-tutorial/service"
 )
 func main(){
-    fmt.Println("Hello World")
+    var list = []model.Student{}
     var student model.Student
-    student.Name = "Khang"
-    fmt.Println(student)
+    
+    student = model.InitStudent("Khang", 22, "191106", "address", 3.8, 162.2)
+    
+    service.AddStudent(student, &list)
+    student.Name= "New Name"
+    service.EditByMSSV(student,"191106", &list)
+    service.PrintSpliceStudent(list)
+    var student2 model.Student
+    student2 = model.InitStudent("Khang", 22, "191107", "address", 2.8, 162.2)
+    
+    service.AddStudent(student2, &list)
+    service.PrintSpliceStudent(list)
+    service.SortWithSelectionSort(&list)
+    // service.DeleteByMSSV(&list, "191106")
+    service.PrintSpliceStudent(list)
+
+
 }
